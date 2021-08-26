e7b0ad6e4c50d77dc821c6fd7f60f58572b4c53c
MichaPotoczny <66637616+yougonnagetyours@users.noreply.github.com>
2021-08-26 09:10:12 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const cars = ["audi","bmw","tesla","vw","skoda","nissan","toyota","volvo","chevrolet","hyundai"]
+
+function includes(array, elementToFind, fromIndex=0) {
+
+  const myArray = [...array];
+  const lll = elementToFind;
+
+  //console.log(myArray)
+
+  return myArray
+}
